857140534cb1c973eaf7b89e8da0b410b17c4f6c
seanc0der <sean.noko@gmail.com>
2023-10-26 13:54:23 +0200

    Added new 'scripts.js' file and solved  the main challenge mini-exercises with the written code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // @ts-check
 
 const provinces = [
@@ -17,6 +18,37 @@ const names = [
 	"Frikkie",
 ];
 
+names.forEach((name) => {
+	console.log(name);
+});
+
+names.forEach((name, index) => {
+	console.log(`${name} (${provinces[index]})`);
+});
+
+const newProvinces = provinces.map((province) => province.toUpperCase());
+console.log(newProvinces);
+
+const lengthOfNames = names.map((name) => name.length);
+console.log(lengthOfNames);
+
+const sortedProvinces = names.toSorted();
+console.log(sortedProvinces);
+
+const filteredProvinces = provinces.filter(
+	(province) => !province.includes("Cape")
+);
+console.log(filteredProvinces.length);
+
+const containsChar = (name) => name.includes("s") || name.includes("S");
+console.log(names.map((name) => [name].some(containsChar)));
+
+const nameCount = names.reduce((accumulator, currentValue, index) => {
+	accumulator[currentValue] = provinces[index];
+	return accumulator;
+}, {});
+
+// Sample data for the additional challenge; to be completed
 const products = [
 	{ product: "banana", price: "2" },
 	{ product: "mango", price: 6 },
