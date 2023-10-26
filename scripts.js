d7ee63d12cc65cad5c9fcdbcb60163415f15c698
seanc0der <sean.noko@gmail.com>
2023-10-26 07:07:15 +0200

    Added script.js file inclusive of the code to be worked on.

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+const provinces = [
+	"Western Cape",
+	"Gauteng",
+	"Northern Cape",
+	"Eastern Cape",
+	"KwaZulu-Natal",
+	"Free State",
+];
+const names = [
+	"Ashwin",
+	"Sibongile",
+	"Jan-Hendrik",
+	"Sifso",
+	"Shailen",
+	"Frikkie",
+];
+
+const products = [
+	{ product: "banana", price: "2" },
+	{ product: "mango", price: 6 },
+	{ product: "potato", price: " " },
+	{ product: "avocado", price: "8" },
+	{ product: "coffee", price: 10 },
+	{ product: "tea", price: "" },
+];
